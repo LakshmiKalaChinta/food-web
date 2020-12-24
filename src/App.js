09e2bd1e3be6f1fe7ed53c63af2a870202acb410
Souravdey777 <piyush.kolkata@gmail.com>
2020-12-24 17:56:02 +0530

    Checkout Added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/logo.jpg";
+import "./App.css";
+import FoodWebPage from "./components/FoodWebPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header">
+        <img className="logo" src={logo} alt="Logo" />
+      </div>
+      <div className="app">
+        <FoodWebPage /></div>
+    </>
   );
 }
 
---
+++ src/components/OrderList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Styles from "./OrderList.module.css";
+import Delete from "./../assets/Delete.png";
+function OrderList(props) {
+    return (
+        <div className={Styles.root}>
+            {console.log(props)}
+            {props.items.map((item, index) =>
+                item.orderCount > 0 ?
+                    <div key={index} data-Name={item.itemName} data-Num={String("0" + (item.orderCount)).slice(-2)} className={Styles.OrderRoot}>
+                        {/* <div> {item.orderCount}</div> */}
+                        <div className={Styles.itemName}>{item.itemName}</div>
+                        <div className={Styles.delete} onClick={() => props.handleDelete(item.itemName)}><img src={Delete} alt="delete" /></div>
+                    </div>
+                    : null)}
+            {/* {props.total} */}
+        </div>
+    );
+}
+
+export default OrderList;
